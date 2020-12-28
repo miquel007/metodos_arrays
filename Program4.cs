c18f6ec85697d684f64fd76847bc6431005b5d8a
Miquel Roig <miquel.roig@urv.cat>
2020-12-28 10:19:32 +0100

    try4

---
+++ Program4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un numero entero!");
+            string Njoke = Console.ReadLine();
+            int num = Convert.ToInt32(Njoke);
+
+           factorial(num);
+
+        }
+
+        public static void factorial(int num)
+        {
+       
+     
+            int op = 1;
+
+            while (0 < num )
+            {
+                op = op * num;
+
+                num--;
+
+            }
+
+            Console.WriteLine("El factorial es " + op);
+        }
+    }
+}
