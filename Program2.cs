b7baebe162ffe897f988a394f17e01deb013bcbd
Miquel Roig <miquel.roig@urv.cat>
2020-12-28 09:57:20 +0100

    try2

---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantos numero quieres generar (enteros)?");
+            string Njoke = Console.ReadLine();
+            int num = Convert.ToInt32(Njoke);
+
+            Console.WriteLine("Valor minimo que quieres generar?");
+            string Mjoke = Console.ReadLine();
+            int min = Convert.ToInt32(Mjoke);
+
+            Console.WriteLine("Valor maximo que quieres generar?");
+            string Ljoke = Console.ReadLine();
+            int max = Convert.ToInt32(Ljoke);
+
+            generador(num, min, max);
+        }
+
+        public static void generador(int num, int min, int max)
+        {
+            var rand = new Random();
+            for (int i=0; i < num; i++)
+                Console.Write("{0,8:N0}", rand.Next(min, max));
+
+        }
+    }
+}
