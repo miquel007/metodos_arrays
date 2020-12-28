b23ad76963b4a119170eb195061fcd793a5271b8
Miquel Roig <miquel.roig@urv.cat>
2020-12-28 10:44:39 +0100

    try6

---
+++ Program6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un numero entero!");
+            string Njoke = Console.ReadLine();
+            int num = Convert.ToInt32(Njoke);
+
+            int  tamaño = contador(num);
+
+            Console.WriteLine("El numero tiene tamaño " + tamaño);
+        }
+
+        public static int contador(int num)
+        {
+            int count = 0;
+            double increment = 10;
+
+            while (increment < num)
+            {
+                count++;
+                increment = Math.Pow(10, count);
+            }
+
+            return (count);
+        }
+    }
+}
