fbcea1eaacc95cb10f8081e1bbaa0c1baa1122f6
Miquel Roig <miquel.roig@urv.cat>
2020-12-28 10:33:01 +0100

    try5

---
+++ Program5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un numero entero!");
+            string Njoke = Console.ReadLine();
+            int num = Convert.ToInt32(Njoke);
+
+            string binario = convertir(num);
+
+            Console.WriteLine("El numero en binari es " + binario);
+        }
+
+        public static string convertir(int num)
+        {
+            bool chek = true;
+            string resultat = "";
+            int count = 2;
+            int op;
+
+            while (0 < num)
+            {
+                op = num % count;
+
+                num = num / 2;
+                if (op == 0)
+                    resultat = "0" + resultat ;
+                else
+                    resultat = "1" + resultat ;
+            }
+
+            return resultat;
+        }
+    }
+}
